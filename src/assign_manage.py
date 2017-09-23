4ca2877359ac1333602e6bf54009c35537c23ef2
Bogdan Rogoz <bogdan@localhost.localdomain>
2017-09-23 16:31:31 +0300

    code modified, added synomyms for operators

--- src/assign_manage.py
+++ src/assign_manage.py
@@ -4,50 +4,35 @@
 # Examples: let x be 5
 #           let avc be "Hello World"
 #           let av = 5
+#           let xyz be 5 multiplied by 4 divided by 2.4
+#                           added to 10 subtracted with 40
+
+import re
 
 class AssignationManager():
-    def __init__(self, params):
-        self.keyword = None
-        self.result = None
-        self.operands = []
-        self.operator = None
-        self.params = params
+    def __init__(self):
+        # only spaces are supported right now
+        # maybe add support for whole \s
+        self.__letComp = re.compile("^let ")
+        self.__eqComp = re.compile(" be ")
+        self.__mulComp = re.compile(" multiplied by | [xX] ")
+        self.__divComp = re.compile(" divided by ")
+        self.__addComp = re.compile(" added to ")
+        self.__subComp = re.compile(" subtracted with ")
 
-    def print_debug(self):
-        print "Keyword:", self.keyword
-        print "Result:", self.result
-        print "Operands:", self.operands
-        print "Operator:", self.operator
-        self.print_operation()
+    def decode(self, msg):
+        msg = re.sub(self.__letComp, "", msg)
+        msg = re.sub(self.__eqComp, " = ", msg)
+        msg = re.sub(self.__mulComp, " * ", msg)
+        msg = re.sub(self.__divComp, " / ", msg)
+        msg = re.sub(self.__addComp, " + ", msg)
+        msg = re.sub(self.__subComp, " - ", msg)
 
-    def print_operation(self):
-        print self.result, "=", self.operands[0]
-        for operand in self.operands[1:-1]:
-            print self.operator, operand
-        if len(self.operands) > 1:
-            print self.operands[-1]
-        
-        # it is not really correct, but it will do for now
-
-    def decode(self):
-        # the first parameters must be "let"
-        self.keyword = self.params[0]
-
-        # then, the next parameter should be the result
-        self.result = self.params[1]
-
-        # then, the 3rd parameter should be "be" or =
-        self.operator = self.params[2]
-        if self.operator in ("be"):
-            self.operator = "="
-
-        # the rest must be processed, as it might be an expression
-        # right now, let's suppose it is just an operand (eg 5 or "Hello World")
-        self.operands.extend(self.params[3:])
+        return msg
 
 
 if __name__ == "__main__":
-    dummy = AssignationManager("let x be 5".split())
-    dummy.decode()
-    dummy.print_debug()
+    dummy = AssignationManager()
+    result = dummy.decode("let xyz be 5")
+    print result
 
